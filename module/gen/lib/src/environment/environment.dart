03d79bc0fddfac796e019ed541293c43318f6102
Deniz Çelik <https://github.com/Deniz-Kmr>
2026-03-23 23:08:54 +0300

    feat: standardize asset module structure

---
+++ module/gen/lib/src/environment/environment.dart
@@ -0,0 +1,25 @@
+enum Flavor { dev, prod }
+
+class Environment {
+  Environment._();
+
+  static Flavor _flavor = Flavor.dev;
+
+  static Flavor get flavor => _flavor;
+
+  static void setFlavor(Flavor flavor) {
+    _flavor = flavor;
+  }
+
+  static String get baseUrl {
+    switch (_flavor) {
+      case Flavor.dev:
+        return 'https://dev-api.gezirehberi.com';
+      case Flavor.prod:
+        return 'https://api.gezirehberi.com';
+    }
+  }
+
+  static bool get isDev => _flavor == Flavor.dev;
+  static bool get isProd => _flavor == Flavor.prod;
+}
--- module/gen/lib/src/index.dart
+++ module/gen/lib/src/index.dart
@@ -1,2 +1,4 @@
 export 'data/index.dart';
 export 'model/dummy_model/index.dart';
+export 'asset/index.dart';
+export 'environment/environment.dart';
